09eff95f6190c4ebeca5b5bd02e62b6daf730f34
Mayank3299 <mayank.a@incubyte.co>
2025-11-06 01:51:41 +0530

    Added AddToCart mutation

--- backend/app/graphql/types/mutation_type.rb
+++ backend/app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :add_to_cart, mutation: Mutations::AddToCart
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
