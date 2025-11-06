d3f888fa262b6548681feaa7efbed355999cfbd6
Mayank3299 <mayank.a@incubyte.co>
2025-11-07 02:53:21 +0530

    Added stock column to product

--- backend/app/models/product.rb
+++ backend/app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   validates_presence_of :name, :price
+  validates_presence_of :stock, numericality: { greater_than: 0 }
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_05_005113) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_06_212042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_05_005113) do
     t.text "description"
     t.string "name"
     t.integer "price"
+    t.integer "stock"
     t.datetime "updated_at", null: false
   end
 
