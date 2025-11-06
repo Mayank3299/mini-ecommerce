800c3fefdb5bd656a5322519068eddb0a44e1857
Mayank3299 <mayank.a@incubyte.co>
2025-11-06 17:40:52 +0530

    Added cart query tests

--- backend/spec/models/cart_item_spec.rb
+++ backend/spec/models/cart_item_spec.rb
@@ -1,19 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
-  it 'is valid with cart, product, and quantity' do
-    cart = Cart.create!
-    product = Product.create!(name: 'T-shirt', price: 499)
-    cart_item = CartItem.new(cart: cart, product: product, quantity: 2)
+  let(:cart) { Cart.create! }
+  let(:product) { Product.create!(name: 'T-shirt', price: 499) }
 
+  it 'is valid with valid attributes' do
+    cart_item = CartItem.new(cart: cart, product: product, quantity: 2)
     expect(cart_item).to be_valid
   end
 
   it 'is invalid without quantity' do
-    cart = Cart.create!
-    product = Product.create!(name: 'T-shirt', price: 499)
     cart_item = CartItem.new(cart: cart, product: product)
-
     expect(cart_item).not_to be_valid
+    expect(cart_item.errors[:quantity]).to include("can't be blank")
+  end
+
+  it 'is invalid without cart' do
+    cart_item = CartItem.new(product: product, quantity: 2)
+    expect(cart_item).not_to be_valid
+    expect(cart_item.errors[:cart]).to include("must exist")
+  end
+
+  it 'is invalid without product' do
+    cart_item = CartItem.new(cart: cart, quantity: 2)
+    expect(cart_item).not_to be_valid
+    expect(cart_item.errors[:product]).to include("must exist")
+  end
+
+  it 'is invalid with zero or negative quantity' do
+    [ 0, -1 ].each do |invalid_qty|
+      cart_item = CartItem.new(cart: cart, product: product, quantity: invalid_qty)
+      expect(cart_item).not_to be_valid
+      expect(cart_item.errors[:quantity]).to include("must be greater than 0")
+    end
   end
 end
--- backend/spec/models/cart_spec.rb
+++ backend/spec/models/cart_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe Cart, type: :model do
     product1 = Product.create!(name: 'T-shirt', price: 500)
     product2 = Product.create!(name: 'Jeans', price: 1000)
 
-    cart_item1 = CartItem.create!(cart: cart, product: product1, quantity: 2)
-    cart_item2 = CartItem.create!(cart: cart, product: product2, quantity: 1)
+    CartItem.create!(cart: cart, product: product1, quantity: 2)
+    CartItem.create!(cart: cart, product: product2, quantity: 1)
 
     expect(cart.cart_items.count).to eq(2)
     expect(cart.cart_items.first.product.name).to eq('T-shirt')
